131e11b934e8174d1d7225df15f81ff7d62a4a9d
Neil Froggatt <neilfr@live.com>
2019-02-28 11:29:01 -0500

    does not post articles to db if they are already there

--- routes/routes.js
+++ routes/routes.js
@@ -17,21 +17,72 @@ db.on("error", function(error) {
   console.log("Database Error:", error);
 });
 
-router.get("/", function(req, res) {
-  //res.send("Hello world");
+// function for executing the scraping process
+function scrapingProcess(existingArticles) {
+  // Make a request via axios for the news section of `tsn.ca`
+  axios.get("https://www.tsn.ca/").then(function(response) {
+    // Load the html body from axios into cheerio
+    var $ = cheerio.load(response.data);
 
-  // call the apiRoutes /all route
+    // For each element with a "article-content" class
+    $(".article-content").each(function(i, element) {
+      // Save the text of the h3 header and .lead class elements... that exist under .article-content elements
+      var articleContent = $(element);
+      var headline = articleContent.find("h3").text();
+      var lead = articleContent.find(".lead").text();
+      var link = articleContent.find("a").attr("href");
+
+      // prepend the tsn base URL if the link does not include a base URL
+      if (link != undefined) {
+        if (link.charAt(0) === "/") {
+          link = "https://www.tsn.ca" + link;
+        }
+      }
+
+      // check if scraped headline is already in the database
+      var newHeadline = true;
+      for (var i = 0; i < existingArticles.length; i++) {
+        if (existingArticles[i].headline === headline) {
+          newHeadline = false;
+        }
+      }
+
+      // if the headline is new, and there is both a headline and a lead, insert the article data into the database
+      if (newHeadline && headline && lead) {
+        var newArticle = {
+          headline: headline,
+          lead: lead,
+          link: link
+        };
+        console.log("-------------------this is going into the database");
+        console.log(newArticle);
+        console.log("-------------------above is going into the database");
+        db.scrapedData.insert(newArticle, function(err, inserted) {
+          if (err) {
+            // Log the error if one is encountered during the query
+            console.log(err);
+          } else {
+            // Otherwise, log the inserted data
+            // console.log(inserted);
+          }
+        });
+      }
+    });
+  });
+}
+
+router.get("/", function(req, res) {
+  // retrieve all articles already in the database
   db.scrapedData.find({}, function(error, found) {
     // Throw any errors to the console
     if (error) {
       console.log(error);
     }
-    // If there are no errors, send the data to the browser as json
+    // If there are no errors, render the data with index.handlebars
     else {
       var hbsObject = {
         articles: found
       };
-      //     console.log(hbsObject);
       res.render("index", hbsObject);
     }
   });
@@ -54,45 +105,18 @@ router.get("/api/all", function(req, res) {
 
 // Scrape data from one site and place it into the mongodb db
 router.get("/api/scrape", function(req, res) {
-  // Make a request via axios for the news section of `ycombinator`
-  axios.get("https://www.tsn.ca/").then(function(response) {
-    // Load the html body from axios into cheerio
-    var $ = cheerio.load(response.data);
-    // For each element with a "article-content" class
-    $(".article-content").each(function(i, element) {
-      // Save the text of the h3 header and .lead class elements under .article-content elements
-      var articleContent = $(element);
-      var headline = articleContent.find("h3").text();
-      var lead = articleContent.find(".lead").text();
-      var link = articleContent.find("a").attr("href");
-      if (link != undefined) {
-        if (link.charAt(0) === "/") {
-          link = "https://www.tsn.ca" + link;
-        }
-      }
-      console.log("href is:");
-      console.log(link);
-
-      // if there is both a headline and a lead, insert them into the database
-      if (headline && lead) {
-        db.scrapedData.insert(
-          {
-            headline: headline,
-            lead: lead,
-            link: link
-          },
-          function(err, inserted) {
-            if (err) {
-              // Log the error if one is encountered during the query
-              console.log(err);
-            } else {
-              // Otherwise, log the inserted data
-              //         console.log(inserted);
-            }
-          }
-        );
-      }
-    });
+  // Get all the existing articles from the database
+  // This will be used to check if scraped articles are already in the database
+  db.scrapedData.find({}, function(error, found) {
+    // Throw any errors to the console
+    if (error) {
+      console.log(error);
+    }
+    // If there are no errors, proceed with scraping process
+    else {
+      // pass the set of existing articles to the scraping process
+      scrapingProcess(found);
+    }
   });
 
   // Send a "Scrape Complete" message to the browser
