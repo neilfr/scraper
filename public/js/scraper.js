1bde5ae14d110baa5d96e5828cfe3a380e2e2cf4
Neil Froggatt <neilfr@live.com>
2019-02-28 19:16:50 -0500

    added article deletion capability

--- public/js/scraper.js
+++ public/js/scraper.js
@@ -1,1 +1,16 @@
 // empty for now
+$(".notesBtn").click(function() {
+  console.log("notes button was clicked!");
+  articleId = $(this).attr("id");
+  console.log(articleId);
+});
+
+$(".deleteBtn").click(function() {
+  console.log("delete button was clicked!");
+  articleId = $(this).attr("id");
+  console.log(articleId);
+  $.ajax({
+    url: "/api/delete/" + articleId,
+    type: "DELETE"
+  });
+});
--- routes/routes.js
+++ routes/routes.js
@@ -64,6 +64,7 @@ function scrapingProcess(existingArticles) {
           } else {
             // Otherwise, log the inserted data
             console.log(inserted);
+            res.end();
           }
         });
       }
@@ -80,11 +81,27 @@ router.get("/", function(req, res) {
     }
     // If there are no errors, render the data with index.handlebars
     else {
+      //console.log(found);
       var hbsObject = {
         articles: found
       };
       res.render("index", hbsObject);
     }
+  });
+});
+/*
+router.delete("/delete/:articleId", function(req, res) {
+  // retrieve all articles already in the database
+  db.scrapedData.remove({ _id: req.params.articleId }, true);
+});
+*/
+router.delete("/api/delete/:articleId", function(req, res) {
+  // retrieve all articles already in the database
+  console.log("req.params.articleId is:");
+  console.log(req.params.articleId);
+  db.scrapedData.remove({ _id: db.ObjectId(req.params.articleId) }, function() {
+    console.log("got to delete route");
+    res.send("Delete Complete");
   });
 });
 
@@ -115,12 +132,63 @@ router.get("/api/scrape", function(req, res) {
     // If there are no errors, proceed with scraping process
     else {
       // pass the set of existing articles to the scraping process
-      scrapingProcess(found);
+      //scrapingProcess(found);
+      // Make a request via axios for the news section of `tsn.ca`
+      axios.get("https://www.tsn.ca/").then(function(response) {
+        // Load the html body from axios into cheerio
+        var $ = cheerio.load(response.data);
+
+        // For each element with a "stack-view" class
+        $(".stack-view").each(function(i, element) {
+          var stackView = $(element);
+          // find the "article-content" class descendant
+          var articleContent = stackView.find(".article-content");
+
+          // within the "article-content" element, find the headline, lead and link
+          var headline = articleContent.find("h3").text();
+          var lead = articleContent.find(".lead").text();
+          var link = articleContent.find("a").attr("href");
+          var newArticle = {
+            headline: headline,
+            lead: lead,
+            link: link
+          };
+
+          // prepend the tsn base URL if the link does not include a base URL
+          if (link != undefined) {
+            if (link.charAt(0) === "/") {
+              link = "https://www.tsn.ca" + link;
+            }
+          }
+
+          // check if scraped headline is already in the database
+          var newHeadline = true;
+          for (var i = 0; i < found.length; i++) {
+            if (found[i].headline === headline) {
+              newHeadline = false;
+            }
+          }
+
+          // if the headline is new, and there is both a headline and a lead, insert the article data into the database
+          if (newHeadline && headline && lead) {
+            db.scrapedData.insert(newArticle, function(err, inserted) {
+              if (err) {
+                // Log the error if one is encountered during the query
+                console.log(err);
+              } else {
+                // Otherwise, log the inserted data
+                console.log(inserted);
+              }
+            });
+          }
+        });
+
+        res.send("Scrape Complete");
+      });
     }
   });
-
   // Send a "Scrape Complete" message to the browser
-  res.send("Scrape Complete");
+  //res.send("Scrape Complete");
 });
 
 module.exports = router;
